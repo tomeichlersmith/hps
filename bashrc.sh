123a52ad592eb7a141b100eb5a13e4c37e2cb565
tomeichlersmith <eichl008@umn.edu>
2022-05-23 10:21:54 -0700

    cleaned up setup script

--- bashrc.sh
+++ bashrc.sh
@@ -105,66 +105,17 @@ shopt -s direxpand
 
 export EDITOR=vim
 
-#Setup Env
-source $HOME/sdf/src/LCIO/setup.sh
-#scl enable devtoolset-6 bash
-source $HOME/sdf/src/root/buildV62202/bin/thisroot.sh
+export HPS_HOME=/sdf/group/hps/users/eichl008/hps
+
+# keep using Cam's java
 export JAVA_HOME=/sdf/group/hps/users/bravo/src/jdk-15.0.1
-export PATH=/sdf/group/hps/users/bravo/.local/bin:/sdf/home/b/bravo/.local/bin:/sdf/group/hps/users/bravo/src/jdk-15.0.1/bin:/sdf/group/hps/users/bravo/src/apache-maven-3.6.3/bin:$PATH
-export LD_LIBRARY_PATH=/sdf/group/hps/users/bravo/.local/lib:$LD_LIBRARY_PATH
+export PATH=/sdf/group/hps/users/bravo/src/jdk-15.0.1/bin:/sdf/group/hps/users/bravo/src/apache-maven-3.6.3/bin:$PATH
+
+#Setup Env
+source ${HPS_HOME}/lcio/setup.sh
+#source ${HPS_HOME}/hps/mc/install/bin/hps-mc-env.sh
+#source ${HPS_HOME}/hps/hpstr/install/bin/setup.sh
+#scl enable devtoolset-6 bash
 
 #Alias to make things easier
-alias c='clear'
-alias v='vim'
-alias r='root -l'
 alias mvnclbd='mvn clean install -DskipTests=true -Dcheckstyle.skip'
-
-alias cmakeconf='cmake -DCMAKE_INSTALL_PREFIX=$HOME/local ..'
-
-alias sshlxplus='ssh -XtY -C lxplus.cern.ch'
-alias sshsvtdaq='ssh -XtY -C rdsrv117'
-alias sshhpsprod='ssh -XtY -C hpsprod@rhel6-64'
-alias sshhpsprodcent='ssh -XtY -C hpsprod@centos7'
-alias sshrhel='ssh -XtY -C rhel6-64'
-
-source ~/sdf/src/hps-mc/install/bin/hps-mc-env.sh
-source ~/sdf/src/hpstr/install/bin/setup.sh
-
-if [ $HOSTNAME == "rdusr219.slac.stanford.edu" ]
-then
-    echo "You are on rdusr219"
-    source /afs/slac.stanford.edu/g/cci/volumes/vol1/sdimages/sdk/V3.3.0/i86-linux-64/tools/envs-sdk.sh
-    #source $HOME/src/root/bin/thisroot.sh
-fi
-
-if [ $HOSTNAME == "rdsrv117" ]
-then
-    echo "You are on rdsrv117"
-    source /afs/slac.stanford.edu/g/cci/volumes/vol1/sdimages/sdk/V3.3.0/i86-linux-64/tools/envs-sdk.sh
-    #source $HOME/src/root/bin/thisroot.sh
-fi
-
-if [[ $HOSTNAME == rhel6-64*  ]]
-then
-    echo "You are on rhel6-64"
-    #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/u/ea/bravo/local/lib
-    export PATH=/u/ea/bravo/local/bin:$PATH
-    export JAVAVER=1.8
-    export JAVA_HOME=/afs/slac/package/java/@sys/jdk1.8
-    #source /nfs/slac/g/hps3/software/setup.sh
-fi
-
-if [[ $HOSTNAME == cent7*  ]]
-then
-    echo "You are on centos7"
-    #source /nfs/slac/g/hps3/users/bravo/src/root/buildV61204/bin/thisroot.sh
-fi
-
-if [[ $HOSTNAME == rdsrv3**  ]]
-then
-    echo "You are on ${HOSTNAME}"
-    source /u1/root/buildV61204/bin/thisroot.sh
-    source /u1/hps/src/LCIO/setup.sh
-    source /u1/hps/nfs/hps3/users/bravo/sw/hpstr/install/bin/setup.sh
-fi
-
