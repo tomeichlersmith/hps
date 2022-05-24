71f6172f1be7d63f1520156f41eb4f888f42b462
tomeichlersmith <eichl008@umn.edu>
2022-05-24 09:40:53 -0700

    next stop on developing bash env

--- bashrc.sh
+++ bashrc.sh
@@ -109,16 +109,29 @@ export HPS_HOME=/sdf/group/hps/users/eichl008/hps
 
 # keep using Cam's java
 export JAVA_HOME=/sdf/group/hps/users/bravo/src/jdk-15.0.1
-export PATH=/sdf/group/hps/users/bravo/src/jdk-15.0.1/bin:/sdf/group/hps/users/bravo/src/apache-maven-3.6.3/bin:$PATH
+export PATH=/sdf/group/hps/users/bravo/src/jdk-15.0.1/bin:/sdf/group/hps/users/bravo/src/apache-maven-3.6.3/bin:$HOME/.local/bin:$PATH
 
 # using Cam's GSL
-export GSL_ROOT_DIR=/sdf/group/hps/users/bravo/src/gsl-2.6/install
+if [ -z $GSL_ROOT_DIR ]; then
+  export GSL_ROOT_DIR=/sdf/group/hps/users/bravo/src/gsl-2.6/install
+  export LD_LIBRARY_PATH=${GSL_ROOT_DIR}/lib:$LD_LIBRARY_PATH
+fi
 
 #Setup Env
-source ${HPS_HOME}/lcio/setup.sh
-#source ${HPS_HOME}/hps/mc/install/bin/hps-mc-env.sh
+if [ -z $LCIO_DIR ]; then
+  export LCIO_DIR=/sdf/group/hps/users/eichl008/hps/lcio/install
+  export LCIO_INCLUDE_DIRS=$LCIO_DIR/include
+  export IO_LCIO_LIBRARY=$LCIO_DIR/lib/liblcio.so
+  export LD_LIBRARY_PATH=$LCIO_DIR/lib:$LD_LIBRARY_PATH
+  export PATH=$LCIO_DIR/bin:$PATH
+  source ${HPS_HOME}/lcio/setup.sh
+fi
+
+if [ -z $HPSMC_DIR]; then
+  source ${HPS_HOME}/mc/install/bin/hps-mc-env.sh
+fi
 #source ${HPS_HOME}/hps/hpstr/install/bin/setup.sh
-#scl enable devtoolset-6 bash
 
 #Alias to make things easier
 alias mvnclbd='mvn clean install -DskipTests=true -Dcheckstyle.skip'
+
