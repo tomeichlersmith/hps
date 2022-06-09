2e0af67833b9a82699ae629d8ff7a3103b0a540f
tomeichlersmith <eichl008@umn.edu>
2022-06-09 11:53:09 -0700

    update cfg and histo/selection json

--- hist_cfg.py
+++ hist_cfg.py
@@ -20,27 +20,15 @@ p.add_library("libprocessors")
 #          Processors         #
 ###############################
 
-vtxana = HpstrConf.Processor('vtxana', 'TridentAnaProcessor')
+ana = HpstrConf.Processor('ana', 'FullTridentTracksAnalyzer')
+ana.parameters["debug"] = 1
 
-###############################
-#   Processor Configuration   #
-###############################
-#Vertex Analysis
-vtxana.parameters["debug"] = 1
-vtxana.parameters["anaName"] = "vtxana"
-vtxana.parameters["cluColl"] = "RecoEcalClusters"
-vtxana.parameters["trkColl"] = "KalmanFullTracks"
-vtxana.parameters["vtxColl"] = "UnconstrainedV0Vertices_KF"
-vtxana.parameters["hitColl"]  = "RotatedHelicalTrackHits"
-vtxana.parameters["mcColl"]  = "MCParticle"
-vtxana.parameters["trkSelectionjson"] = os.environ['HPSTR_BASE']+'/analysis/selections/tridents/trackSelection-2pt3gev.json'
-vtxana.parameters["vtxSelectionjson"] = os.environ['HPSTR_BASE']+'/analysis/selections/tridents/tridentSelection-2pt3gev.json'
-#vtxana.parameters["histoCfg"] = os.environ['HPSTR_BASE']+"/analysis/plotconfigs/tracking/vtxAnalysis.json"
-vtxana.parameters["histoCfg"] = os.environ['HPSTR_BASE']+"/analysis/plotconfigs/tridents/triAnalysis-2pt3gev.json"
-vtxana.parameters["beamE"] = 2.3
-vtxana.parameters["isData"] = options.isData
+ana.parameters["event_selection"] = os.environ['HPS_HOME']+"/trident-event-selection.json"
+ana.parameters["histo_cfg"] = os.environ['HPS_HOME']+"/track_histos-2.3GeV.json"
+ana.parameters["beamE"] = 2.3
+ana.parameters["isData"] = options.isData
+
 CalTimeOffset=-999.
-
 if (options.isData==1):
     CalTimeOffset=56.
     print("Running on data file: Setting CalTimeOffset %d"  % CalTimeOffset)
@@ -50,30 +38,9 @@ elif (options.isData==0):
 else:
     print("Specify which type of ntuple you are running on: -t 1 [for Data] / -t 0 [for MC]")
 
+ana.parameters["CalTimeOffset"]=CalTimeOffset
 
-vtxana.parameters["CalTimeOffset"]=CalTimeOffset
-
-#Region definitions
-
-RegionPath=os.environ['HPSTR_BASE']+"/analysis/selections/tridents/"
-#vtxana.parameters["regionDefinitions"] = [RegionPath+'Tight.json']
-#vtxana.parameters["regionDefinitions"] = ['./tridentL1L1.json','./tridentL1L2.json','./tridentL2L1.json','./tridentL2L2.json']
-#vtxana.parameters["regionDefinitions"] = ['cfgs2019/tridentAllLayerCombos.json']
-vtxana.parameters["regionDefinitions"] = [RegionPath+'/tridentAllLayerCombos-BothClusters.json',
-                                          RegionPath+'/tridentAllLayerCombos-BothClusters-RadCut.json',
-                                          RegionPath+'/tridentL1L1-BothClusters.json',
-                                          RegionPath+'/tridentL1L1-BothClusters-RadCut.json',
-                                          RegionPath+'/tridentL1L2-BothClusters.json',
-                                          RegionPath+'/tridentL1L2-BothClusters-RadCut.json',
-                                          RegionPath+'/tridentL2L1-BothClusters.json',
-                                          RegionPath+'/tridentL2L1-BothClusters-RadCut.json',
-                                          RegionPath+'/tridentL2L2-BothClusters.json',
-                                          RegionPath+'/tridentL2L2-BothClusters-RadCut.json'
-]
-
-vtxana.parameters["regionWABDefinitions"]=[RegionPath+'/wabL1ElectronCluster.json']
-# Sequence which the processors will run.
-p.sequence = [vtxana]
+p.sequence = [ana]
 
 p.printProcess()
 
