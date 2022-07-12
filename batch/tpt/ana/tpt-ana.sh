a569ce1903e2554b82637662176e753b5221f92e
tomeichlersmith <eichl008@umn.edu>
2022-07-12 07:33:12 -0700

    update output log format and input slcio directory

--- batch/tpt/ana/tpt-ana.sh
+++ batch/tpt/ana/tpt-ana.sh
@@ -2,12 +2,18 @@
 #
 #SBATCH --partition=shared
 #SBATCH --ntasks=1
-#SBATCH --output=output-%j.txt
+#SBATCH --output=%A-%a.out
+#
+# Filename Pattern Quick Ref:
+#   %J - job id number (universal)
+#   %a - array number
+#   %A - master number for arrays
 #
 #SBATCH --job-name=tpt-ana
 #
 # the 2016-subsample Cam made available for me is
 #   Run 007800 Partitions 0 - 387 (inclusive)
+# we could change this to be something defined on the comand line
 #SBATCH --array=0-20
 
 srun hpstr ${HPS_HOME}/hist_cfg.py -t 1 \
--- batch/tpt/tuplize/tpt-tuplize.sh
+++ batch/tpt/tuplize/tpt-tuplize.sh
@@ -2,7 +2,7 @@
 #
 #SBATCH --partition=shared
 #SBATCH --ntasks=1
-#SBATCH --output=output-%j.txt
+#SBATCH --output=%A-%a.out
 #
 # maximum job time of six hours
 #SBATCH --time 6:00:00
@@ -15,7 +15,7 @@
 #   Run 007800 Partitions 0 - 387 (inclusive)
 #SBATCH --array=0-20
 
-hps_2016_subsample_data_dir=${HPS_HOME}/2016-subsample/reco/
+hps_2016_subsample_data_dir=/sdf/group/hps/data/physrun2016/hps_007800/reco/hash8d32e68be1b/
 
 srun hpstr ${HPS_HOME}/tuplize_cfg.py -t 1 \
   -i ${hps_2016_subsample_data_dir}/hps_007800_${SLURM_ARRAY_TASK_ID}.slcio \
