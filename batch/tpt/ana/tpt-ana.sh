08ace93a884e9339577c75a40a2961595077204a
tomeichlersmith <eichl008@umn.edu>
2022-09-14 09:01:24 -0700

    expand analysis to all partitions I have tuplized

--- batch/tpt/ana/tpt-ana.sh
+++ batch/tpt/ana/tpt-ana.sh
@@ -14,7 +14,7 @@
 # the 2016-subsample Cam made available for me is
 #   Run 007800 Partitions 0 - 387 (inclusive)
 # we could change this to be something defined on the comand line
-#SBATCH --array=0-20
+#SBATCH --array=0-387
 
 srun hpstr ${HPS_HOME}/hist_cfg.py -t 1 \
   -i ${HPS_HOME}/2016-subsample/tuples/tuple_2016_subsample_run_007800_part_$(printf "%04d" ${SLURM_ARRAY_TASK_ID}).root
