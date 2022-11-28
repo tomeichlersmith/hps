249bd1630b4db2e90c55bba2056075fc9a017512
tomeichlersmith <eichl008@umn.edu>
2022-11-28 08:28:21 -0600

    updating run script

--- alignment/run.sh
+++ alignment/run.sh
@@ -2,11 +2,17 @@
 __usage__() {
   cat <<\HELP
     
-    Run the tracking (both KF and GBL) over both iterations of the 2019SensorSurvey detector
+    Run the tracking (both KF and ST) over the nominal and intentionally misaligned 2019 detectors.
 
   USAGE:
     
-    ./run.sh
+    bash run.sh [-h|--help|help|-?] TAG
+
+  ARGUMENTS:
+    TAG : short string to identify this run of tracking and mille
+
+  OPTIONS:
+    -h, --help, help, -? : Print this and exit
 
 HELP
 }
@@ -14,35 +20,52 @@ HELP
 __run__() {
   local trk=$1
   local det=$2
+  local tag=$3
 
   java \
     -DdisableSvtAlignmentConstants \
     -XX:+UseSerialGC \
     -Xmx5000m \
-    -jar /export/scratch/users/eichl008/hps/java/distribution/target/hps-distribution-5.1-SNAPSHOT-bin.jar \
+    -jar /export/scratch/users/eichl008/hps/java/distribution/target/hps-distribution-5.2-SNAPSHOT-bin.jar \
     -R 10716 \
-    -d ${det} \
-    -DoutputFile=${trk}/${det}/${trk}_${det} \
+    -d ${det}_${trk} \
+    -DoutputFile=${trk}/${det}/${tag} \
     tracking_${trk}_alignment.lcsim \
     -i fee_recon_20um120nA_200.slcio
   return $?
 }
 
 __main__() {
+  local tag=""
   case $1 in
     -h|--help|help|-?)
       __usage__
       return 0
       ;;
+    -*)
+      echo "ERROR: Unrecognized option '$1'"
+      return 1
+      ;;
+    *)
+      tag="$1"
+      ;;
   esac
 
-  for det in HPS_Nominal_2019SensorSurvey_iter0 HPS_2019_L1ty100um_iter0; do
-    for trk in gbl kf; do
-      echo -n "${trk} ${det} ..."
-      mkdir -p ${trk}/${det} 
-      __run__ ${trk} ${det} &> ${trk}/${det}/${trk}_${det}_run.log
-      echo "done"
-    done
+  if [ -z ${tag} ]; then
+    echo "ERROR: Need to provide tag to distinguish this run."
+    return 1
+  fi
+
+  dets=( HPS_2019_L1ty100um_iter4 HPS_2019_L1ty100um_iter5 )
+  trks=( st kf )
+
+  for i in "${!dets[@]}"; do
+    det=${dets[$i]}
+    trk=${trks[$i]}
+    echo -n "$(date) ${trk} ${det} ..."
+    mkdir -p ${trk}/${det} 
+    __run__ ${trk} ${det} ${tag} &> ${trk}/${det}/${tag}_run.log
+    echo "done"
   done
 }
 
