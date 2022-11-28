01646c0e7686c43e36316dd40389ef912089b94f
tomeichlersmith <eichl008@umn.edu>
2022-11-28 12:33:27 -0600

    add method for calculating and drawing mean

--- alignment/_differ.py
+++ alignment/_differ.py
@@ -3,6 +3,8 @@
 import uproot
 import matplotlib.pyplot as plt
 import os
+import math
+from scipy.stats import norm
 
 class Differ :
     """Differ allowing easy comparison of "similar" files
@@ -62,6 +64,16 @@ class Differ :
             return ax.hist((edges[1:]+edges[:-1])/2, bins=edges, weights=uproot_obj.values(), **kwargs)
         else :
             return ax.hist(uproot_obj.array(library='pd').values, **kwargs)
+    
+    def __calc_mean(values, weights) :
+        """Calculate the mean and standard deviation of the input values with the input weights
+        
+        This is the default method for including the means in the legend labels if no
+        callable is provided.
+        """
+        mean = (weights*values).sum()/(weights.sum())
+        stdd = math.sqrt((weights*(values-mean)**2).sum()/(weights.sum()))
+        return mean, stdd
         
     def keys(self, *args, **kwargs) :
         """Call keys on the first file provided
@@ -72,12 +84,13 @@ class Differ :
         return self.files[0][0].keys(*args, **kwargs)
 
     def plot1d(self, column, xlabel, 
-              ylabel = None,
-              yscale = 'log',
-              ylim = (None,None),
-              out_dir = None, file_name = None,
-              legend_kw = dict(),
-              **hist_kwargs) :
+               ylabel = None,
+                yscale = 'log',
+                ylim = (None,None),
+                out_dir = None, file_name = None,
+                legend_kw = dict(),
+                include_mean = None, draw_mean = False,
+               **hist_kwargs) :
         fig = plt.figure('differ',figsize=(11,8))
         ax = fig.subplots()
         
@@ -86,8 +99,20 @@ class Differ :
         if 'linewidth' not in hist_kwargs :
             hist_kwargs['linewidth'] = 2
 
+        legend_labels = []
         for f, name, style in self.files :
-            Differ.__plt_hist(ax, f[column], label=name, **style, **hist_kwargs)
+            weights, bins, p = Differ.__plt_hist(ax, f[column], **style, **hist_kwargs)
+            if include_mean is not None :
+                values = (bins[1:]+bins[:-1])/2
+                if callable(include_mean) :
+                    mean, stdd = include_mean(values, weights)
+                else :
+                    mean, stdd = Differ.__calc_mean(values, weights)
+                legend_labels.append(f'{name} $\mu = {mean*1000:.1f}\mu m$\n$\sigma = {stdd*1000:.1f}\mu m$')
+                if draw_mean :
+                    plt.plot(values, norm.pdf(values, mean, stdd), **style, label='_nolegend_')
+            else :
+                legend_labels.append(name)
 
         ax.set_xlabel(xlabel)
         ax.set_ylabel(ylabel)
@@ -95,7 +120,7 @@ class Differ :
         ax.set_ylim(*ylim)
         if 'title' not in legend_kw :
             legend_kw['title'] = self.grp_name
-        ax.legend(**legend_kw)
+        ax.legend(labels = legend_labels, **legend_kw)
 
         if out_dir is None :
             plt.show()
@@ -103,5 +128,7 @@ class Differ :
             fn = column
             if file_name is not None :
                 fn = file_name
-            fig.savefig(os.path.join(out_dir,fn)+'.pdf', bbox_inches='tight')
+            full_name = os.path.join(out_dir, fn)
+            os.makedirs(os.path.dirname(full_name), exist_ok=True)
+            fig.savefig(full_name+'.pdf', bbox_inches='tight')
             fig.clf()
