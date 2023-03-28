3e727b6ad81ad53a0e3d90a343ff5c8365bb3652
tomeichlersmith <eichl008@umn.edu>
2023-03-28 10:34:04 -0500

    enter-env.sh script

---
+++ enter-env.sh
@@ -0,0 +1,7 @@
+apptainer run \
+  --env "PS1=${PS1}" \
+  --env "LS_COLORS=${LS_COLORS}" \
+  --hostname hps-env.$(uname -n) \
+  --home /export/scratch/users/eichl008/hps \
+  hps-env-v2.0.0.sif \
+  /bin/bash -i # make shell interactive
