ec919ae8a8c945d7a7f6383727089991dd8a4b2c
tomeichlersmith <eichl008@umn.edu>
2023-05-02 15:06:20 -0700

    some more files to back up

--- enter-env.sh
+++ enter-env.sh
@@ -1,7 +1,7 @@
-apptainer run \
+singularity run \
   --env "PS1=${PS1}" \
   --env "LS_COLORS=${LS_COLORS}" \
   --hostname hps-env.$(uname -n) \
-  --home /export/scratch/users/eichl008/hps \
-  ${1:-hps-env-v2.0.0.sif} \
+  --home $(pwd -P) \
+  ${1:-hps-env-v3.0.0.sif} \
   /bin/bash -i # make shell interactive
