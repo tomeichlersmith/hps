970e9f37fb67958fdfc9b2277b8ad4787e0dd88e
tomeichlersmith <eichl008@umn.edu>
2023-05-04 07:56:42 -0700

    put the pede run onto worker nodes

---
+++ 2016-align/pede/run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/scl enable devtoolset-8 -- /bin/bash
+#SBATCH --ntasks=1
+#SBATCH --time=24:00:00
+#SBATCH --partition=shared
+
+work_root="/sdf/group/hps/users/eichl008/hps/2016-align"
+detname="$1"
+hps-mc-job run \
+  -d ${work_root}/${detname}/pede/scratch \
+  -c ${work_root}/pede/run.cfg \
+  pede ${work_root}/${detname}/pede/job.json
