506439f59553fafe453fe749921f105a0dd77898
tomeichlersmith <eichl008@umn.edu>
2023-06-02 08:40:36 -0700

    bind hps data directory'

--- enter-env.sh
+++ enter-env.sh
@@ -1,6 +1,7 @@
 singularity run \
   --env "PS1=${PS1}" \
   --env "LS_COLORS=${LS_COLORS}" \
+  --bind /sdf/group/hps \
   --hostname hps-env.$(uname -n) \
   --home $(pwd -P) \
   ${1:-hps-env-v3.0.0.sif} \
