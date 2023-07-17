c71a51f9071920c21249a6b71085220a219a617b
tomeichlersmith <eichl008@umn.edu>
2023-07-17 10:16:15 -0500

    delete enter env in favor of denv

--- enter-env.sh
+++
@@ -1,8 +0,0 @@
-singularity run \
-  --env "PS1=${PS1}" \
-  --env "LS_COLORS=${LS_COLORS}" \
-  --bind /sdf/group/hps \
-  --hostname hps-env.$(uname -n) \
-  --home $(pwd -P) \
-  ${1:-hps-env-v3.2.0.sif} \
-  /bin/bash -i # make shell interactive
