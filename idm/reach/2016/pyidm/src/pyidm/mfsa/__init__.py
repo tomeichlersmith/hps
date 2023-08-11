28472354834ec9bf798f17778852ccd7d57c2cb5
tomeichlersmith <eichl008@umn.edu>
2023-08-11 10:54:18 -0500

    add postprocess to mfsa.run
    - patch up prettyprint wrapper to use tqdm.autonotebook
    - rename loader to preprocess to generalize its meaning

--- idm/reach/2016/pyidm/src/pyidm/mfsa/__init__.py
+++ idm/reach/2016/pyidm/src/pyidm/mfsa/__init__.py
@@ -11,15 +11,16 @@ def _quiet_wrapper(pool_iter, **kwargs):
 
 
 def _pretty_wrapper(pool_iter, total=None, **kwargs):
-    import tqdm
-    return tqdm.tqdm(pool_iter, total=total)
+    from tqdm.autonotebook import tqdm
+    return tqdm(pool_iter, total=total)
 
 
 def run(
     processor: Callable,
     work_items: Iterable,
     *,
-    loader: Optional[Callable] = None,
+    preprocess: Optional[Callable] = None,
+    postprocess: Optional[Callable] = None,
     ncores: Optional[int] = None,
     quiet: Optional[bool] = False,
 ):
@@ -33,11 +34,18 @@ def run(
         result objects that are accumulatble
     work_items: Iterable
         iterable that contains items to include in the analysis
-    loader: Optional[Callable]
-        function that loads data from a single work item into
-        in-memory data that can be handled by processor
-        default is no loading function, expecting the work items
-        to be prepared as the processor expects
+    preprocess: Optional[Callable]
+        function that is called *on each work item* and whose
+        return value is passed into process. This can be used to
+        prepatory tasks that are shared across many analyses like
+        loading the data from disk into memory.
+    postprocess: Optional[Callable]
+        function that is called on the resulting accumulated analysis
+        result before returning it from this function. This can be
+        helpful for doing common post-analysis tasks like unpacking
+        accumulator objects into their wrapped values. It should
+        operate on the passed object *in place* since its return value
+        is ignored.
     ncores: Optional[int]
         number of cores to use, defaults to number of cores of
         the current machine
@@ -46,8 +54,8 @@ def run(
     """
 
     work_function = processor
-    if loader is not None:
-        work_function = lambda item: processor(loader(item))
+    if preprocess is not None:
+        work_function = lambda item: processor(preprocess(item))
 
     wrapper = _quiet_wrapper if quiet else _pretty_wrapper
 
@@ -61,4 +69,8 @@ def run(
                 total=len(work_items)
             )
         )
+    
+    if postprocess is not None:
+        postprocess(ana_result)
+    
     return ana_result
